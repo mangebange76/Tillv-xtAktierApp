4d32b209f410e054b18715aab02df9800e24e9f5
mangebange76 <magnusberg47@gmail.com>
2025-07-03 06:12:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
-[server]
-headless = true
-enableCORS=false
-port = $PORT
+[general]\n\
+email = \"din@email.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+port = 8501\n\
+enableCORS = false\n\
 " > ~/.streamlit/config.toml
